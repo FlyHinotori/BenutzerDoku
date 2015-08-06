0c250dada35ea3efa62fafa3c8b36d7f697666a3
Torsten Noack <torsten@schlummweg.de>
2015-08-06 20:29:57 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -58,7 +58,7 @@ source_suffix = ['.rst', '.md']
 master_doc = 'index'
 
 # General information about the project.
-project = 'SphinxSample'
+project = 'BenutzerDoku FlyHinotori'
 copyright = '2015, PowerShell'
 author = 'PowerShell'
 
